4e552afec1c1bcb417c51ce62e86064c2325a78d
dev_sm <tmdals9974@naver.com>
2023-09-01 22:02:15 +0900

    feat: 4강 quiz

---
+++ src/4.Template-literals/quiz.ts
@@ -0,0 +1,15 @@
+export type Expect<T extends true> = T;
+export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
+        T,
+    >() => T extends Y ? 1 : 2
+    ? true
+    : false;
+
+// function getProtocol(url: unknown) {
+function getProtocol(url: `${string}://${string}`) {
+    return url.split(":")[0];
+}
+
+getProtocol("http://typescriptlang.org");
+// @ts-expect-error
+getProtocol("typescriptlang.org");
