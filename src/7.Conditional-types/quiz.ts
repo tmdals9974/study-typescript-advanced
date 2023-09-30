93cc6395fef0595e1a5375f14be59a99fcf044f1
dev_sm <tmdals9974@naver.com>
2023-09-30 09:14:14 +0900

    feat: 7강 quiz

---
+++ src/7.Conditional-types/quiz.ts
@@ -0,0 +1,17 @@
+export type Expect<T extends true> = T;
+export type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false;
+
+type UserPath = ["users", ":id"];
+type UserOrgPath = ["users", ":id", "orgs", ":orgId"];
+
+// type ExtractPathParams<T extends string[]> = unknown;
+type ExtractPathParams<T extends string[]> = {
+  [K in T[number] as K extends `:${infer T1}` ? T1 : never]: string;
+};
+
+type A = ExtractPathParams<UserPath>;
+
+type tests = [
+  Expect<Equal<ExtractPathParams<UserPath>, { id: string }>>,
+  Expect<Equal<ExtractPathParams<UserOrgPath>, { id: string; orgId: string }>>
+];
